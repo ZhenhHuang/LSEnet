69662e55a8d822702c41502db1f269322e8cccf9
RiemanGraph <127167919+ZhenhHuang@users.noreply.github.com>
2024-01-28 13:56:11 +0800

    Update eval_utils.py

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -101,50 +101,48 @@ class cluster_metrics:
         self.predicts = predicts
 
     def clusterAcc(self):
-        l1 = list(set(self.trues))
-        l2 = list(set(self.predicts))
-        num1 = len(l1)
-        num2 = len(l2)
-        ind = 0
-        if num1 != num2:
-            for i in l1:
-                if i in l2:
-                    pass
-                else:
-                    self.predicts[ind] = i
-                    ind += 1
-        l2 = list(set(self.predicts))
-        num2 = len(l2)
-        if num1 != num2:
-            raise "class number not equal!"
-        """compute the cost of allocating c1 in L1 to c2 in L2"""
-        cost = np.zeros((num1, num2), dtype=int)
-        for i, c1 in enumerate(l1):
-            maps = [i1 for i1, e1 in enumerate(self.predicts) if e1 == c1]
-            for j, c2 in enumerate(l2):
-                maps_d = [i1 for i1 in maps if self.predicts[i1] == c2]
-                cost[i, j] = len(maps_d)
+        # best mapping between true_label and predict label
+        l1 = list(set(self.true_label))
+        numclass1 = len(l1)
 
-        mks = Munkres()
+        l2 = list(set(self.pred_label))
+        numclass2 = len(l2)
+        if numclass1 != numclass2:
+            print('Class Not equal, Error!!!!')
+            return 0, 0, 0, 0, 0, 0, 0
+
+        cost = np.zeros((numclass1, numclass2), dtype=int)
+        for i, c1 in enumerate(l1):
+            mps = [i1 for i1, e1 in enumerate(self.true_label) if e1 == c1]
+            for j, c2 in enumerate(l2):
+                mps_d = [i1 for i1 in mps if self.pred_label[i1] == c2]
+
+                cost[i][j] = len(mps_d)
+
+        # match two clustering results by Munkres algorithm
+        m = Munkres()
         cost = cost.__neg__().tolist()
-        index = mks.compute(cost)
-        new_predicts = np.zeros(len(self.predicts))
+
+        indexes = m.compute(cost)
+
+        # get the match results
+        new_predict = np.zeros(len(self.pred_label))
         for i, c in enumerate(l1):
-            c2 = l2[index[i][1]]
-            allocate_index = [ind for ind, elm in enumerate(self.predicts) if elm == c2]
-            new_predicts[allocate_index] = c
-        self.new_predicts = new_predicts
-        acc = metrics.accuracy_score(self.trues, new_predicts)
-        f1_macro = metrics.f1_score(self.trues, new_predicts, average='macro')
-        precision_macro = metrics.precision_score(
-            self.trues, new_predicts, average='macro')
-        recall_macro = metrics.recall_score(
-            self.trues, new_predicts, average='macro')
-        f1_micro = metrics.f1_score(self.trues, new_predicts, average='micro')
-        precision_micro = metrics.precision_score(
-            self.trues, new_predicts, average='micro')
-        recall_micro = metrics.recall_score(
-            self.trues, new_predicts, average='micro')
+            # correponding label in l2:
+            c2 = l2[indexes[i][1]]
+
+            # ai is the index with label==c2 in the pred_label list
+            ai = [ind for ind, elm in enumerate(self.pred_label) if elm == c2]
+            new_predict[ai] = c
+
+        self.new_predict = new_predict
+        acc = metrics.accuracy_score(self.true_label, new_predict)
+        f1_macro = metrics.f1_score(self.true_label, new_predict, average='macro')
+        precision_macro = metrics.precision_score(self.true_label, new_predict, average='macro')
+        recall_macro = metrics.recall_score(self.true_label, new_predict, average='macro')
+        f1_micro = metrics.f1_score(self.true_label, new_predict, average='micro')
+        precision_micro = metrics.precision_score(self.true_label, new_predict, average='micro')
+        recall_micro = metrics.recall_score(self.true_label, new_predict, average='micro')
         return acc, f1_macro, precision_macro, recall_macro, f1_micro, precision_micro, recall_micro
 
     def evaluateFromLabel(self, use_acc=False):
